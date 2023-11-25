e36bad4533b887dc6ffc42896bb801d27ce01945
Nathaniel Maffly <ncmaffly@ucdavis.edu>
2023-11-25 15:35:57 -0800

    added subgenre to broad genre mapping

---
+++ retrieve_broad_genre.py
@@ -0,0 +1,24 @@
+import json
+
+def load_genre_data(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def create_lookup_table(data):
+    lookup_table = {}
+    for broad_genre, subgenres in data.items():
+        for subgenre in subgenres:
+            lookup_table[subgenre.lower()] = broad_genre
+    return lookup_table
+
+def get_broad_genre(subgenre, lookup_table):
+    return lookup_table.get(subgenre, "Unknown Genre")
+
+file_path = 'sorted_genres_for_search.json'
+genre_data = load_genre_data(file_path)
+genre_lookup_table = create_lookup_table(genre_data)
+
+input_subgenre = "Abstract Beats"
+broad_genre = get_broad_genre(input_subgenre, genre_lookup_table)
+print(f"The broad genre for '{input_subgenre}' is '{broad_genre}'.")
--- webApp.py
+++ webApp.py
@@ -1,13 +1,15 @@
-from flask import Flask, redirect, request, session, render_template
+from flask import Flask, redirect, request, session, render_template, g
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
 from collections import Counter
 from genre_fashion_mapping import genre_fashion  # Import the genre-fashion mapping
+from retrieve_broad_genre import genre_lookup_table, get_broad_genre
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
+import time
 
 load_dotenv()
 
@@ -17,6 +19,19 @@ SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
 SCOPE = 'user-library-read user-top-read'
 
 sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE)
+
+@app.before_request
+def before_request():
+    g.start_time = time.time()
+
+# Add an after_request hook to measure the time taken for each request and log the information
+@app.after_request
+def after_request(response):
+    if hasattr(g, 'start_time'):
+        elapsed_time = time.time() - g.start_time
+        app.logger.info(f"Request took {elapsed_time:.5f} seconds")
+    return response
+
 
 @app.route('/')
 def index():
@@ -52,12 +67,30 @@ def get_top_genres():
         top_artists = sp.current_user_top_artists(limit=50, time_range='short_term')['items']
         genres = extract_genres(sp, top_artists)
         top_genres = Counter(genres).most_common(10)
-        
+        print(top_genres)
+        genre_strings = [genre_tuple[0] for genre_tuple in top_genres]
+        broad_genres = []
+        for genre in top_genres:
+            broad_genre = get_broad_genre(genre[0], genre_lookup_table)
+            broad_genre_tuple = (broad_genre, genre[1])
+            
+            genre_exists = False
+
+            for i, (_genre, _count) in enumerate(broad_genres):
+                if _genre == broad_genre_tuple[0]:
+                    # Update the count in the tuple
+                    broad_genres[i] = (_genre, _count + broad_genre_tuple[1])
+                    genre_exists = True
+                    break  # Break the loop once the update is done
+                    
+            if not genre_exists:
+                broad_genres.append(broad_genre_tuple)
+
         avatar_description = generate_avatar_description(top_genres)
         
-        return render_template('top_genres.html', top_genres=top_genres, avatar_description=avatar_description)
+        return render_template('top_genres.html', top_genres=top_genres, avatar_description=avatar_description, broad_genres=broad_genres)
     except Exception as e:
-        print(e)
+        print(f"An exception occurred: {str(e)}")
         return redirect('/')
 
 def extract_genres(sp, top_artists):
