96e6a8015bbca4284620ad32acfc045b22a865c3
Nathaniel Maffly <ncmaffly@ucdavis.edu>
2023-11-25 00:34:41 -0800

    updated flask on webApp.py, deleted old_app.py

--- webApp.py
+++ webApp.py
@@ -1,56 +1,78 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, redirect, request, session, render_template
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
+from dotenv import load_dotenv
+from collections import Counter
+from genre_fashion_mapping import genre_fashion  # Import the genre-fashion mapping
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
-client_id = os.getenv('SPOTIFY_CLIENT_ID')
-client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+load_dotenv()
 
-# Define the SpotifyOAuth instance
-sp_oauth = SpotifyOAuth(client_id, client_secret, redirect_uri=os.getenv('SPOTIFY_REDIRECT_URI'), scope='user-library-read user-top-read')
+SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
+SCOPE = 'user-library-read user-top-read'
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        scope=SCOPE)
 
 @app.route('/')
-def show_top_tracks():
-    user, top_tracks = fetch_top_tracks()
-    html_content = generate_html(user, top_tracks)
-    return render_template('index.html', content=html_content)
-
-def fetch_top_tracks():
-    if 'token_info' not in session or sp_oauth.is_token_expired(session['token_info']):
-        return None, None
-
-    sp = spotipy.Spotify(auth=session['token_info']['access_token'])
-    user = sp.current_user()
-    top_tracks = sp.current_user_top_tracks(limit=10, time_range='medium_term')
-    return user, top_tracks
+def index():
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
+    sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
 @app.route('/callback')
 def callback():
-    token_info = sp_oauth.get_access_token(request.args['code'])
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
     session['token_info'] = token_info
-    return redirect(url_for('show_top_tracks'))
+    return redirect('/get_top_genres')
 
-def generate_html(user, top_tracks):
-    if user is None or top_tracks is None:
-        return "<h1>Error: Unable to fetch top tracks. Please try again.</h1>"
+def generate_avatar_description(top_genres):
+    avatar_description = []
+    for genre, _ in top_genres:
+        fashion_items = genre_fashion.get(genre.lower(), [])
+        avatar_description.extend(fashion_items)
+    return avatar_description
 
-    html_content = f"<h1>Welcome, {user['display_name']}!</h1>"
-    for idx, track in enumerate(top_tracks['items']):
-        track_info = f"{idx + 1}. {track['name']} by {', '.join([artist['name'] for artist in track['artists']])}"
-        html_content += f"<p>{track_info}</p>"
+@app.route('/get_top_genres')
+def get_top_genres():
+    try:
+        token_info = session.get('token_info', None)
+        if not token_info:
+            raise Exception("User not logged in")
+        sp = spotipy.Spotify(auth=token_info['access_token'])
+        top_artists = sp.current_user_top_artists(limit=50, time_range='short_term')['items']
+        genres = extract_genres(sp, top_artists)
+        top_genres = Counter(genres).most_common(10)
+        
+        avatar_description = generate_avatar_description(top_genres)
+        
+        return render_template('top_genres.html', top_genres=top_genres, avatar_description=avatar_description)
+    except Exception as e:
+        print(e)
+        return redirect('/')
 
-    return html_content
+def extract_genres(sp, top_artists):
+    genres = []
+    for artist in top_artists:
+        artist_genres = sp.artist(artist['id'])['genres']
+        genres.extend(artist_genres)
+    return genres
 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
